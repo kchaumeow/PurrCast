40a08675d0c7390c0487eebc3aa58e8dbb1b697c
kchaumeow <atrofimova516@gmail.com>
2023-08-29 16:21:44 +0300

    feat: main imports and settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+const router = createBrowserRouter([
+  {
+    element: <Home />,
+    index: true,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <RouterProvider router={router}/>
+    </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>There will be a Weather web-app</div>;
+}
